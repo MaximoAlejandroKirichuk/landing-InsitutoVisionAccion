4b74d6264fae226a869c9d948316a1cf8134fd23
maxik <maximoalejandro.kirichuk@alumnos.uai.edu.ar>
2026-07-13 18:08:36 -0300

    fix(deploy): configure Astro for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +8,5 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
 });
